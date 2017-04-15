04c01a1f5ba94fc1e04dbc9eeb7b3bc8bb132004
hobbitpl <minimalnie@gmail.com>
2017-04-15 10:07:42 +0200

    1st

---
+++ hobbit_dual_home_BIG.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1280x800 --pos 1920x128 --rotate normal --output DP1 --off --output DP2 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --primary --mode 1920x1080 -r 75 --pos 0x0 --rotate normal
